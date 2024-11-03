2ba99da473cf7173f39bbd84a2a9b0d8a0670691
phantom":) <106024626+Touhidulislam622@users.noreply.github.com>
2024-11-03 22:57:53 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./componrnts/Hero";
+import Navigation from "./componrnts/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
